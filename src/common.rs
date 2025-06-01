5120b85f090c24a7eb6cc285b22ee717bca3fd28
KongBaiDeFeng <hn461467105@sina.com>
2025-06-02 01:25:36 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://111.59.69.78:21114".to_owned()
+    "http://oltwhxz.dynv6.net:21114".to_owned()
 }
 
 #[inline]
